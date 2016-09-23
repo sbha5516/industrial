eb13c0ea5e4c6d40038a1b13ef77a26334703cdb
Swagato Bhatta <sbhatta@novaentertainment.com.au>
2016-09-23 20:58:56 +1000

    Version 1

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-09-23 10:13:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-09-23T10:13:10+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
---
+++ templates/it_oxygen/js/template.js
@@ -0,0 +1,9 @@
+(function ($) {
+    // Move the Offcanvas Toggle button in the Header
+    $(document).ready(function () {
+        $("div.g-offcanvas-toggle:not(.offcanvas-toggle-particle)").prependTo($("#g-header"));
+    });
+
+
+   // baguetteBox.run('.gallery');
+})(jQuery);
