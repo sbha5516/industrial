ed8b31cc7ffa063846f652a8b22dc134141059a1
Swagato Bhatta <sbhatta@novaentertainment.com.au>
2016-09-25 01:46:27 +1000

    FIxed mobile menu and pushing some unpushed files

--- templates/it_oxygen/js/template.js
+++ templates/it_oxygen/js/template.js
@@ -1,9 +1,27 @@
 (function ($) {
+    'use strict';
+
+
+    jQuery.each($('.slide'), function (index, val) {
+        $(this).find('img').css('opacity', '0');
+        $(this).find('.bt_img').css({
+            'background-repeat': 'no-repeat',
+            'background-size': 'contain',
+            'background-position': 'center center',
+            'min-width': '960px',
+            'height': '600px',
+            'background-image': 'url("' + $(this).find('img').prop('src') + '")'
+        });
+    });
+
     // Move the Offcanvas Toggle button in the Header
     $(document).ready(function () {
         $("div.g-offcanvas-toggle:not(.offcanvas-toggle-particle)").prependTo($("#g-header"));
     });
 
 
-   // baguetteBox.run('.gallery');
+    // baguetteBox.run('.gallery');
 })(jQuery);
+
+
+
