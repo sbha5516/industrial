587fac1fe17903705c6a8ef7a2ea9d034152ffc5
Swagato Bhatta <sbhatta@novaentertainment.com.au>
2016-09-26 01:39:12 +1000

    Made slider image responsive , fixed many minor alignment and style issues throughout

--- templates/it_oxygen/js/template.js
+++ templates/it_oxygen/js/template.js
@@ -3,13 +3,9 @@
 
 
     jQuery.each($('.slide'), function (index, val) {
+        $(this).find('.bt_img').addClass('slider-image');
         $(this).find('img').css('opacity', '0');
         $(this).find('.bt_img').css({
-            'background-repeat': 'no-repeat',
-            'background-size': 'contain',
-            'background-position': 'center center',
-            'min-width': '960px',
-            'height': '600px',
             'background-image': 'url("' + $(this).find('img').prop('src') + '")'
         });
     });
